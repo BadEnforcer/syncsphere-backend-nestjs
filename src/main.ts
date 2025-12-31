45b6af381525fc6504bc83b6ec002d0d0adb7c9e
Raj Dwivedi <rajdwivedipc@gmail.com>
2025-12-31 23:51:35 +0530

    better-auth now works

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './redis/adapter/adapter.service';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './auth';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +12,13 @@ async function bootstrap() {
 
   // cors
   app.enableCors({origin: '*'})
+
+  // Register Better Auth handler for all auth routes
+  const expressApp = app.getHttpAdapter().getInstance();
+  const handler = toNodeHandler(auth);
+  expressApp.all('/api/auth/*path', (req: Request, res: Response) => {
+    return handler(req, res);
+  });
 
   // Use Redis Adapter for Socket.io
   const redisIoAdapter = new RedisIoAdapter(app);
